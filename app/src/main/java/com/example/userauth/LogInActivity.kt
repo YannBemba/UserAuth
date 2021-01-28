0c1c3dc9cfd45a0dc4b4c9a3dd36e5432ae991e4
Yann Bemba Longa <yannbembacontact@gmail.com>
2021-01-28 14:59:29 +0100

    Firebase Auth

--- app/src/main/java/com/example/userauth/LogInActivity.kt
+++ app/src/main/java/com/example/userauth/LogInActivity.kt
@@ -1,18 +1,78 @@
 package com.example.userauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.userauth.databinding.ActivityLogInBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class LogInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.btnInscription.setOnClickListener {
+            registerUser()
+        }
+
     }
 
+    private fun registerUser() {
+
+        val email = binding.tilEmail.editText?.text.toString()
+        val mdp = binding.tilMdp.editText?.text.toString()
+
+        if(email.isNotEmpty() && mdp.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try{
+                    mAuth.createUserWithEmailAndPassword(email, mdp).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.IO){
+                        Toast.makeText(this@LogInActivity, e.message, Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun checkLoggedInState() {
+        if(mAuth.currentUser == null) {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Erreur de connexion")
+                .setMessage("Vous n'êtes pas inscrit ")
+                .show()
+            val intent = Intent(this@LogInActivity, SignInActivity::class.java)
+            startActivity(intent)
+        } else {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Connexion réussie")
+                .setMessage("Vous êtes bien connecté")
+                .setPositiveButton("OK"){ dialog, which ->
+
+                }
+                .show()
+        }
+    }
 
 }
--- app/src/main/java/com/example/userauth/SignInActivity.kt
+++ app/src/main/java/com/example/userauth/SignInActivity.kt
@@ -1,16 +1,83 @@
 package com.example.userauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.userauth.databinding.ActivitySignInBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.btnConnexion.setOnClickListener {
+            loginUser()
+        }
+
     }
+
+    private fun loginUser() {
+
+        val email = binding.tilEmailCo.editText?.text.toString()
+        val mdp = binding.tilMdpCo.editText?.text.toString()
+
+        if(email.isNotEmpty() && mdp.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try{
+                    mAuth.signInWithEmailAndPassword(email, mdp).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.IO){
+                        Toast.makeText(this@SignInActivity, e.message, Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLoggedInState()
+    }
+
+    private fun checkLoggedInState() {
+        if(mAuth.currentUser == null) {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Erreur de connexion")
+                .setMessage("Vous n'êtes pas inscrit ")
+                .show()
+            //val intent = Intent(this@Sign)
+
+        } else {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Connexion réussie")
+                .setMessage("Vous êtes bien connecté")
+                .setPositiveButton("OK"){ dialog, which ->
+
+                }
+                .show()
+        }
+    }
+
 }
